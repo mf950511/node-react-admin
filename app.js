b6d83d81bab110da5f357c2871427e436e961fb8
francismeng <francismeng@xwfintech.com>
2020-06-12 11:30:13 +0800

    feat: 添加登录接口

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-09 08:17:58
+ * @LastEditTime: 2020-06-12 11:25:05
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \react-admin-node\app.js
+ */ 
 const path = require('path')
 const Koa = require('koa')
 const app = new Koa()
 const koaSession = require('koa-session')
 const router = require('./router')
 const serve = require('koa-static')
+const koaBody = require('koa-body')
 const sessionMiddleWares = require('./middlewares/session')
 const { sessionConfig } = require('./config')
 
@@ -11,6 +20,7 @@ app.
 use(serve(path.resolve(__dirname, 'static'))).
 use(koaSession(sessionConfig, app)).
 use(sessionMiddleWares).
+use(koaBody({ "formLimit":"5mb", "jsonLimit":"5mb", "textLimit":"5mb" })).
 use(router.routes()).
 use(router.allowedMethods())
 
--- router/user.js
+++ router/user.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-09 08:39:03
+ * @LastEditTime: 2020-06-12 11:25:50
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \react-admin-node\router\user.js
+ */ 
 const Router = require('koa-router')
 const router = new Router({
   prefix: '/user'
@@ -16,6 +24,20 @@ router.get('/asd', async (ctx, next) => {
 router.get('/a', async (ctx, next) => {
   const sessionData = await ctx.sessions.get()
   ctx.body = sessionData 
+})
+
+router.post('/login', async (ctx, next) => {
+  const body = ctx.request.body
+  const sessionId = await ctx.sessions.set(body)
+  ctx.session.sessionId = sessionId
+  ctx.body = {
+    code: 0,
+    data: {
+      ...body,
+      sessionId
+    },
+    msg: '登陆成功'
+  }
 })
 
 module.exports = router.routes()
